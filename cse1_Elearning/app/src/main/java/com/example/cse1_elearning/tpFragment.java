a50750ff24d7f8e128fe35c522efd413c30d7290
karimunnisask6 <karimunnisa4424@gmail.com>
2024-08-30 12:16:20 +0530

    Elearning

---
+++ cse1_Elearning/app/src/main/java/com/example/cse1_elearning/tpFragment.java
@@ -0,0 +1,26 @@
+package com.example.cse1_elearning;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
+public class tpFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_tp,container,false);
+        WebView wv1=view.findViewById(R.id.webtp);
+        wv1.loadUrl("https://www.tutorialspoint.com/");
+        return view;
+    }
+}
