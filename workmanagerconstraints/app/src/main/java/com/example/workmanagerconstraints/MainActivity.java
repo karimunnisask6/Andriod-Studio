de6a031a0e192c6c5c769d74eda3bfe1fdcf242e
karimunnisask6 <karimunnisa4424@gmail.com>
2024-09-25 18:08:08 +0530

    work manager constraints

---
+++ workmanagerconstraints/app/src/main/java/com/example/workmanagerconstraints/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.workmanagerconstraints;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.work.Constraints;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btn=(Button) findViewById(R.id.btn);
+        btn=findViewById(R.id.btn);
+        Constraints constraints=new Constraints.Builder().setRequiresCharging(true).build();
+        WorkRequest workRequest=new OneTimeWorkRequest.Builder(myworker.class).setConstraints(constraints).build();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WorkManager.getInstance(getApplicationContext()).enqueue(workRequest);
+
+            }
+        });
+
+    }
+
+
+}
--- workmanagerconstraints/app/src/main/java/com/example/workmanagerconstraints/myworker.java
+++ workmanagerconstraints/app/src/main/java/com/example/workmanagerconstraints/myworker.java
@@ -1,2 +1,24 @@
-package com.example.workmanagerconstraints;public class myworker {
+package com.example.workmanagerconstraints;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class myworker extends Worker {
+    public myworker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        for(int i=0;i<10000;i++){
+            Log.i("Tagy","count is:"+ i);
+        }
+        return Result.success();
+
+}
 }
